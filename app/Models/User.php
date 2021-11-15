6cd451890420418a006dcef4742f16d3bfe430bb
Alina Zahovora <alina.zahovora@nure.ua>
2021-11-15 23:23:50 +0200

    Create sign-in with database, create models.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,115 +8,17 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    private $name;
-    private $surname;
-    private $email;
-    private $password;
-    private $google_id;
-    private $facebook_id;
+    private $Name;
+    private $Surname;
+    private $Email;
+    private $Phone;
+    private $Address;
+    private $Password;
+    private $Role;
 
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
 
-    /**
-     * @param mixed $name
-     */
-    public function setName($name): void
-    {
-        $this->name = $name;
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getSurname()
-    {
-        return $this->surname;
-    }
-
-    /**
-     * @param mixed $surname
-     */
-    public function setSurname($surname): void
-    {
-        $this->surname = $surname;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param mixed $email
-     */
-    public function setEmail($email): void
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param mixed $password
-     */
-    public function setPassword($password): void
-    {
-        $this->password = $password;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getGoogleId()
-    {
-        return $this->google_id;
-    }
-
-    /**
-     * @param mixed $google_id
-     */
-    public function setGoogleId($google_id): void
-    {
-        $this->google_id = $google_id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFacebookId()
-    {
-        return $this->facebook_id;
-    }
-
-    /**
-     * @param mixed $facebook_id
-     */
-    public function setFacebookId($facebook_id): void
-    {
-        $this->facebook_id = $facebook_id;
-    }
-
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+    protected $table = 'User';
 }
